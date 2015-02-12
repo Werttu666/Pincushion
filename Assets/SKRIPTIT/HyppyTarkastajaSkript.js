cf63b17ec242a94f977ebc5655f88b41c1dc4ed9
Werttu666 <werttu_666@hotmail.com>
2015-02-12 13:59:45 +0200

    Versio 0.04
    Animut valmiin

---
+++ Assets/SKRIPTIT/HyppyTarkastajaSkript.js
@@ -0,0 +1,19 @@
+﻿function Start() {
+	pelaaja = GameObject.FindGameObjectsWithTag ("Player");
+	Physics2D.IgnoreCollision(pelaaja.collider2D, collider2D);
+}
+
+function OnTriggerEnter2D (other : Collider2D) {
+	print ("kosket maahan");
+	TYYNY2D.tiput = 0;
+}
+
+function OnTriggerStay2D (other : Collider2D) {
+	TYYNY2D.voiHypata = 1;
+	TYYNY2D.tiput = 0;
+}
+
+function OnTriggerExit2D (other : Collider2D) {
+	TYYNY2D.voiHypata = 0;
+	TYYNY2D.tiput = 1;
+}
---
+++ Assets/SKRIPTIT/MAINHAHMO/TYYNY2D.js
@@ -0,0 +1,55 @@
+﻿var speed = 15.0;
+var jumpSpeed = 500.0;
+
+var myAnim : Animator;
+
+static var voiHypata = 0;
+static var tiput = 1;
+
+
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+
+
+	var hor = Input.GetAxis("Horizontal");
+
+	rigidbody2D.velocity = Vector2(speed*hor,rigidbody2D.velocity.y);
+	
+	if(hor > 0.1 ){
+		myAnim.SetFloat("MeeOikeelle",1.0);
+	}
+	else {
+		myAnim.SetFloat("MeeOikeelle",0);
+	}
+	if(hor < -0.1 ){
+		myAnim.SetFloat("MeeVasempaan",1.0);
+	}
+	else {
+		myAnim.SetFloat("MeeVasempaan",0);
+	}
+}
+
+function Update(){
+	if(Input.GetButtonDown("Jump") && voiHypata == 1){
+		myAnim.SetTrigger("Jumping");
+		rigidbody2D.AddForce(Vector2(0.0,jumpSpeed));
+	}
+	if(tiput == 1){
+		myAnim.SetBool("Falling", true);
+	}
+	if(tiput == 0){
+		myAnim.SetBool("Falling", false);
+	}
+}
+
+function Tiput(){
+	myAnim.SetBool("Falling", true);
+}
+
+function TipuitMaahan(){
+	myAnim.SetTrigger("Tipuit");
+}
