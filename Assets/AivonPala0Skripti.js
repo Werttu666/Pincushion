21d379c72a94f63a2368cda2121e7a868cd0f9bc
Werttu666 <werttu_666@hotmail.com>
2015-02-13 13:44:02 +0200

    Versio 0.05
    Aivo pieru

---
+++ Assets/AivonPala0Skripti.js
@@ -0,0 +1,17 @@
+﻿
+function OnTriggerStay2D (other:Collider2D){
+	if (other.gameObject.name == "Paloina0"){
+		AivoSkripti.aivoPala00paikalla = 1;
+		print("Pala paikallaan"); 
+	}
+	else{
+		AivoSkripti.aivoPala00paikalla = 0;
+		print("Pala pois");
+	}
+}
+function OnTriggerExit2D (other:Collider2D){
+	if (other.gameObject.name == "Paloina0"){
+		AivoSkripti.aivoPala00paikalla = 0;
+		print("Pala pois");
+	}
+}
---
+++ Assets/SKRIPTIT/PALAPELI/MovePoint2.js
@@ -0,0 +1,27 @@
+﻿ private var screenPoint: Vector3;
+ private var offset: Vector3;
+ private var curScreenPoint : Vector3;
+ private var curPosition : Vector3;
+     
+ function Start () {
+ }
+ 
+ function Update () {
+ }
+ 
+ function OnMouseDown () {
+     screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+     offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+     Screen.showCursor = false;
+     }
+     
+ function OnMouseDrag() { 
+     curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+     curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+     transform.position = curPosition;
+     }
+ 
+ function OnMouseUp(){
+     Screen.showCursor = true;
+     }
+ 
