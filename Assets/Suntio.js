c9645da99931acde8bd282e1c82d7b90656436dd
Werttu666 <werttu_666@hotmail.com>
2015-02-25 14:58:12 +0200

    Versio 0.15

---
+++ Assets/Suntio.js
@@ -0,0 +1,17 @@
+﻿static var lyo = 0;
+static var jahtaa = 0;
+
+var speed = 25.0;
+
+var suntioAnim : Animator;
+
+
+function Update () {
+
+	target = gameObject.FindWithTag("Player").transform;
+	transform.LookAt(Vector3(target.position.x,transform.position.y,target.position.z));
+	
+	if(jahtaa == 1){
+		rigidbody2D.velocity = Vector2(100,rigidbody2D.velocity.y);
+	}
+}
